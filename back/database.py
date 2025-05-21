e2598c7f5b02e6a62f453940f7e774c8def9662a
Emanuel M <emanuel.munera1@gmail.com>
2025-05-21 16:40:13 -0500

    Minor change to fetch url

--- back/database.py
+++ back/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:mysql1475@localhost:3306/event_attendance"
+URL_DATABASE = "mysql+pymysql://root:root@localhost:3306/event_attendance"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
